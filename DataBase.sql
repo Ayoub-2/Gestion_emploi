4fe852e90f63a64be27b6c8cc680ca6289e529a3
med-boubekri <76815001+med-boubekri@users.noreply.github.com>
2021-04-29 22:12:53 +0000

    Update DataBase.sql

--- DataBase.sql
+++ DataBase.sql
@@ -1,19 +1,13 @@
-create table Niveau(
-	ID  number(2),
-	nom varchar(30),
-	constraint PK_NIV primary key(ID) 
-);
-
 create table Etudiant(
 	ID  		number(5),
+	ID_niv		number(2),
 	nom 		varchar(30),
 	prenom		varchar(30),
 	email 		varchar(50),
 	genre 		char(10),
 	adresse 	varchar(70),
-	ID_niv		number(2),
 	constraint PK_ETUD primary key(ID),
-	constraint FK_NIV foreign key(ID_niv) references Niveau(ID)
+	constraint FK_ETUD_NIV foreign key(ID_niv) references Niveau(ID)
 );
 
 create table Formation(
@@ -22,42 +16,74 @@ create table Formation(
 	constraint PK_FORM primary key(ID) 
 );
 
+create table paiement(
+	ID number(5),
+	montantVerse number(5,3),
+	recu VARCHAR(512) CHARACTER SET 'ascii' COLLATE 'ascii_general_ci' NOT NULL,
+	observation varchar(200) 
+);
+
 create table Inscription(
 	ID_et  number(5),
 	ID_form  number(5),
-	nom varchar(30),
-	constraint PK_INS primary key(ID_et, ID_form),
+	ID_paiement  number(5),
+	fraisFormation number(5,3),
+	anneeUniversitaire Date,
+	niveau varchar(20),
+	constraint PK_INS primary key(ID_et, ID_form, ID_paiement),
 	constraint FK_INS_FORM foreign key(ID_form) references formation(ID),
 	constraint FK_INS_ETUD foreign key(ID_et) references Etudiant(ID)
+	constraint FK_INS_PAIEMENT foreign key(ID_paiment) references Paiement(ID)
 );
 
-create table Matiere(
+create table Element(
 	ID  number(5),
-	ID_PROF number(5),
+	ID_ENS number(5),
+	ID_MODULE number(5),
 	nom varchar(30),
-	constraint PK_MAT primary key(ID),
-	constraint FK_PROF foreign key(ID_PROF) references Professeur(ID)
+	constraint PK_ELEM primary key(ID),
+	constraint FK_ELEM_ENS foreign key(ID_ENS) references Enseignant(ID),
+	constraint FK_ELEM_MOD foreign key(ID_MODULE) references Module(ID)
 );
 
-create table Formation_Matiere(
-	ID_mat  number(5),
-	ID_form  number(5),
-	nom varchar(30),
-	constraint PK_FM primary key(ID_et, ID_form),
-	constraint FK_FM_FORM foreign key(ID_form) references formation(ID),
-	constraint FK_FM_MAT foreign key(ID_mat) references Matiere(ID),
+create table Vacation(  --contraintes et MLD
+	ID   number(5), 
+	taux 	number(2,3),
+	taxImpotRevenu number(2,3),
+	jour Date,
+	montant number(5,3),
+	montantBrutArrandi number(5,3),
+	IR number(2,3),
+	IR_arrandi number(2,3),
+	net number(5,3)
 );
 
-create table Professeur(
+create table Module(
+	ID  number(5),
+	ID_FORM number(5),
+	nom varchar(30),
+	constraint PK_MOD primary key(ID),
+	constraint FK_MOD_FORM foreign key(ID_FORM) references Formation(ID)
+);
+
+create table Enseignant(
 	ID  		number(5),
+	ID_grade	number(2),
+	ID_Dep	number(2),
 	nom 		varchar(30),
 	prenom		varchar(30),
 	email 		varchar(50),
 	type 		varchar(20),
 	salaire 	Number(6,3),
-	ID_grade	number(2),
-	constraint PK_PROF primary key(ID),
-	constraint FK_GRADE foreign key(ID_grade) references Grade(ID)
+	constraint PK_ENS primary key(ID),
+	constraint FK_ENS_GRADE foreign key(ID_grade) references Grade(ID),
+	constraint FK_ENS_DEP foreign key(ID_Dep) references Departement(ID)
+);
+
+create table Departement(
+	ID  number(2),
+	nom varchar(30),
+	constraint PK_GRADE primary key(ID)
 );
 
 create table Grade(
@@ -68,35 +94,20 @@ create table Grade(
 
 --=======================================
 
-create table Categorie(
+create table Rubrique(
 	ID 					Number(3),
-	Libelle 			varchar(30),
-	Designation			Varchar(50),
-	constraint PK_CAT primary key(ID)
-);
-
-create table rubrique(
-	ID 					Number(3),
-	ID_cat 				Number(2),
+	ID_PEmp 			Number(2),
 	Libelle 			varchar(30),
 	Date_rubrique		date,
 	montant 			number(10,4),
 	constraint PK_RUB primary key(ID),
-	constraint FK_CAT foreign key(ID_cat) references Categorie(ID)
+	constraint FK_RUB_PEmp foreign key(ID_PEmp) references ProgrammeEmp(ID)
 );
-create table Table_Emploi(
-	ID 				Number(3),
-	intitule 			varchar(30),
-	Responsable			Varchar(50),
-	Exercice            Date,
-	ID_form             number(5),
-	constraint PK_CAT primary key(ID),
-	constraint FK_CAT foreign key(ID_form) references Formation(ID)
-);
-create table Emploi_Catgeorie(
-	ID_categorie   Number(5),
-	ID_emploi      Number(5),
-	constraint  PK_EM_CAT primary key (ID_categorie , ID_emploi),
-	constraint FK_EC_Emp foreign key(ID_emploi) references Table_Emploi(ID),
-	constraint FK_EC_CAT foreign key(ID_categorie) references Categorie(ID)
+
+create table ProgrammeEmp(
+	ID 					Number(3),
+	Intitule			varchar(50),
+	Responsable			varchar(30),
+	Exercice			date,
+	constraint PK_RUB primary key(ID),
 );
