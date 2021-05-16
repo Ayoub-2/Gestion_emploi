22e2df2d47dae9b8e74fd41bcb1eb25ab86288c6
med-boubekri <76815001+med-boubekri@users.noreply.github.com>
2021-05-16 23:52:17 +0100

    Add files via upload

---
+++ java/com/pfa/login/Login.java
@@ -0,0 +1,34 @@
+package com.pfa.login;
+
+import java.io.IOException;  
+import java.io.PrintWriter;  
+  
+import javax.servlet.RequestDispatcher;  
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;  
+import javax.servlet.http.HttpServletRequest;  
+import javax.servlet.http.HttpServletResponse;  
+
+@WebServlet("/login")
+public class Login extends HttpServlet { 
+public void doPost(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {  
+	
+    PrintWriter out = response.getWriter();  
+          
+    String u=request.getParameter("username");
+    String p=request.getParameter("userpass");
+    boolean result = new LoginDao().validate(u, p);
+    if(result){
+    	response.sendRedirect("admin/index.html"); 
+    }  
+    else{  
+        System.out.println("Sorry username or password error");  
+        RequestDispatcher rd=request.getRequestDispatcher("index.html");  
+        rd.include(request,response);
+    }  
+          
+    out.close();  
+    }   
+}  
